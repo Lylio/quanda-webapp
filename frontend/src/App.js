92f2fa64ee36a05907b6b763017bfa1bc074dd28
Lyle Christine <cxz@gmx.com>
2019-09-17 21:27:41 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import {Route} from "react-router";
 import NavBar from './NavBar/NavBar';
 import Questions from './Questions/Questions';
 import Question from './Question/Question';
+import Callback from './Callback';
+import NewQuestion from './NewQuestion/NewQuestion';
+import SecuredRoute from './SecuredRoute/SecuredRoute';
 import LyleComponent from "./LyleComponent/LyleComponent";
 
 class App extends Component {
@@ -12,6 +15,8 @@ class App extends Component {
                 <NavBar/>
                 <Route exact path='/' component={Questions}/>
                 <Route exact path='/question/:questionId' component={Question}/>
+                <Route exact path='/callback' component={Callback}/>
+                <SecuredRoute path='/new-question' component={NewQuestion} />
                 <LyleComponent/>
             </div>
         );
