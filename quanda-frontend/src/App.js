adffaf05f0f86aa14c8711ee859137de3baac8b6
Lylio <cxz@gmx.com>
2020-09-23 21:46:33 +0100

    update

--- quanda-frontend/src/App.js
+++ quanda-frontend/src/App.js
@@ -6,7 +6,6 @@ import Question from './Question/Question';
 import Callback from './Callback';
 import NewQuestion from './NewQuestion/NewQuestion';
 import SecuredRoute from './SecuredRoute/SecuredRoute';
-import LyleComponent from "./LyleComponent/LyleComponent";
 
 class App extends Component {
     render() {
@@ -17,7 +16,6 @@ class App extends Component {
                 <Route exact path='/question/:questionId' component={Question}/>
                 <Route exact path='/callback' component={Callback}/>
                 <SecuredRoute path='/new-question' component={NewQuestion} />
-                <LyleComponent/>
             </div>
         );
     }
